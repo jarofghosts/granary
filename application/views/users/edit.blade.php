02c25f0232c8aa4bac1beb56a5e53d1a78ce5132
jesse keane <me@jessekeane.me>
2012-09-23 00:58:21 -0700

    Minor stuffs, haven't even tested

--- application/views/users/edit.blade.php
+++ application/views/users/edit.blade.php
@@ -14,25 +14,32 @@ Edit {{ $user->display_name }}
 @endsection
 
 @section('main_content')
-<form method="post" action="{{ URL::base() }}/users/edit" id="user_edit_post">
+    <div class="avatar_box">
+        <img src="{{ URL::base() }}{{ $user->avatar }}" alt="{{ $user->display_name }}" class="mini_avatar"/><br/>
+        <a class="button avatar-toggle" href="{{ URL::base() }}/users/edit/$user->id }}/avatar">change avatar</a><br/>
+        <form id="avatar-form" style="display: none" method="post" action="{{ URL::base() }}/users/change_avatar" enctype="multipart/form-data">
+            <input type="text" name="avatar" placeholder="Avatar" value="{{ $user->edit_avatar }}"/>
+            <input type="file" name="disabled" placeholder="Avatar" style="display: none"/><br/>
+            <a href="#" class="button avatar-type-toggle">upload instead</a> <button type="submit" class="button">link avatar</button>
+        </form>
+    </div>
+<form id="user_profile_edit" action="{{ URL::base() }}/users/save_profile" method="post">
+    <br/>
     <input type="hidden" name="id" value="{{ $user->id }}"/>
-    @if ( $change_avatar != 1 )
-    <img src="{{ URL::base() }}{{ $user->avatar }}" alt="{{ $user->display_name }}"/><br/>
-    <a class="button" href="{{ URL::base() }}/users/edit/{{ $user->id }}/avatar">change avatar</a><br/>
-    @else
-    <input type="text" name="avatar" placeholder="Avatar" value="{{ $user->avatar }}"/><br/>
-    @endif
+    <input type="hidden" name="avatar" value="{{ $user->edit_avatar }}"/>
     <input type="password" name="password" placeholder="Change Password"/><br/>
     <input type="email" name="email" placeholder="E-mail" value="{{ $user->email }}"/><br/>
-    <input type="text" name="name" placeholder="Real Name" value="{{ $user->name }}"/><br/>
+    <input type="text" name="real_name" placeholder="Real Name" value="{{ $user->real_name }}"/><br/>
     <input id="color_picker" type="text" name="color" placeholder="Color" value="{{ $user->color }}"/><br/>
     <textarea id="about_me_textarea" style="background-color: {{ $user->color }}" name="about_me" placeholder="About Me">{{ $user->about_me }}</textarea><br/>
-    <button type="submit">Save User</button>
+    <button type="submit">save</button>
 </form>
 @endsection
 
 @section('post_includes')
+<script src="{{ URL::base() }}/js/plugins/jquery.form.js"></script>
 <script src="{{ URL::base() }}/js/plugins/colorpick/jquery.miniColors.min.js"></script>
+<script src="{{ URL::base() }}/js/user_edit.js"></script>
 <script>
 
     $("#color_picker").miniColors( {
