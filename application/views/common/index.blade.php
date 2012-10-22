a76edacf17e9af876f115b89d1881ad821683ed2
jesse keane <jarofghosts@ziggy.(none)>
2012-10-22 10:32:51 -0700

    fixed links, name

--- application/views/common/index.blade.php
+++ application/views/common/index.blade.php
@@ -1,17 +1,17 @@
 @layout('common.template')
 
 @section('title')
-Codename Sailor
+Granary
 @endsection
 
 @section('main_content')
-This is codename: sailor.<br/>
+This is Granary.<br/>
 
 @if (Auth::check())
 <a href="{{ URL::base() }}/posts/new">Make a new post</a>.. or <a href="{{ URL::base() }}/logout">logout</a>.<br/>
 Also, possibly <a href="{{ URL::base() }}/categories/new">make a new category</a> or <a href="{{ URL::base() }}/posts/">Look at some posts</a>
 @else
-You pretty much need to <a href="{{ URL::base() }}/users/login">login</a> to do anything.<br/>
+You pretty much need to <a href="{{ URL::base() }}/login">login</a> to do anything.<br/>
 If you don't already have an account, try to <a href="{{ URL::base() }}/login">login</a> anyway. Or, if you're feeling more official, you can always 
 <a href="{{ URL::base() }}/users/new">register an account</a> like a big boy.
 @endif
